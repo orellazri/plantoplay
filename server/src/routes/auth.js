bcb3948863798b3c57277865be3bc5be4386ce39
Orel <ryiseld@gmail.com>
2021-09-12 17:23:15 +0300

    Add express validator and implement in register

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,13 +1,25 @@
 const router = require("express").Router();
+const { body, validationResult } = require("express-validator");
 
 const db = require("../db");
 
-router.get("/register", async (req, res, next) => {
-  try {
-    res.json({ message: "register" });
-  } catch (err) {
-    next(err);
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 5 }),
+  body("display_name").isLength({ min: 3 }),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new Error("Invalid input");
+      }
+
+      res.json({ message: "register" });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 module.exports = router;
