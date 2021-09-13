b556c2f701a10951ac1b23393871b595c1894884
Orel <ryiseld@gmail.com>
2021-09-13 15:16:43 +0300

    Add logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,9 +3,11 @@ import Link from "./core/Link";
 
 function Navbar() {
   return (
-    <div className="flex bg-gray-600 p-5 space-x">
+    <div className="flex bg-gray-800 p-4 space-x">
       <div className="flex-1">
-        <Link to="/">PlanToPlay</Link>
+        <Link to="/">
+          <img src="logo-dpad.png" className="w-10" />
+        </Link>
       </div>
       <div className="flex-none">
         <div className="flex flex-row space-x-4">
