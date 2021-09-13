5bbe7d9974452eba5b12faa255f6ea0d6111cf5b
Orel <ryiseld@gmail.com>
2021-09-13 16:08:12 +0300

    Implement login post call

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
+import axios from "axios";
+
 import Input from "../components/core/Input";
 import Button from "../components/core/Button";
 
 function LoginPage() {
+  const [error, setError] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     setEmail(email.trim());
-    if (!email || !password || password.length <= 5) {
+    if (!email || !password) {
       return;
+    }
+
+    try {
+      const result = await axios.post("/auth/login", { email, password });
+      const { token } = result.data;
+      console.log(token);
+    } catch (err) {
+      console.log(err);
+      setError("Invalid credentials.");
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="w-full max-w-md px-8 pt-6 pb-8 mx-auto bg-gray-700 rounded shadow-md">
+        {error && <div className="">{error}</div>}
         <div className="mb-4">
           <label className="block mb-2 text-sm font-bold">Email Address</label>
           <Input type="email" value={email} required autoFocus onChange={(e) => setEmail(e.target.value)} />
