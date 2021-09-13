de16330b0abc16ac3c6da259dfaae96569e045a3
Orel <ryiseld@gmail.com>
2021-09-13 16:00:44 +0300

    Add readme files and axios defaults

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
+
+import Config from "./config.json";
 import "./index.css";
 import App from "./App";
+
+axios.defaults.baseURL = Config.SERVER_URL;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ server/README.md
@@ -0,0 +1,7 @@
+# Node Server for Plan to Play
+
+## Steps to run
+
+- Rename `.env.example` to `.env` and update accordingly
+- Run knex migrations with `knex migrate:latest`
+- Run with `yarn start` or `yarn dev`
