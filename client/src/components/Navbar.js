25d25e99189717287abbb542a9c662445ccfcf4f
Orel <ryiseld@gmail.com>
2021-09-14 10:30:44 +0300

    Add max width to container and navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,17 +3,19 @@ import Link from "./core/Link";
 
 function Navbar() {
   return (
-    <div className="flex p-4 bg-gray-800 space-x">
-      <div className="flex-1">
-        <Link to="/">
-          <img src="logo-dpad.png" className="w-10" alt="Plan to Play Logo" />
-        </Link>
-      </div>
-      <div className="flex flex-row space-x-4">
-        <Button to="/login">Login</Button>
-        <Button to="/register" outline>
-          Sign Up
-        </Button>
+    <div className="p-4 bg-gray-800">
+      <div className="flex mx-auto space-x max-w-7xl">
+        <div className="flex-1 mx-auto">
+          <Link to="/">
+            <img src="logo-dpad.png" className="w-10" alt="Plan to Play Logo" />
+          </Link>
+        </div>
+        <div className="flex flex-row space-x-4">
+          <Button to="/login">Login</Button>
+          <Button to="/register" outline>
+            Sign Up
+          </Button>
+        </div>
       </div>
     </div>
   );
