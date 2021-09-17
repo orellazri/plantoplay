198e884db47b76b072ee2277757eaaf3b3ec9d93
Orel <ryiseld@gmail.com>
2021-09-17 11:34:11 +0300

    Add igdb to .env.example

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function SearchPage({ match }) {
+  useEffect(() => {
+    console.log("Searching...");
+  }, []);
+
   return <div>Search page. Searching for {match.params.title}</div>;
 }
 
