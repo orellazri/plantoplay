a2747fb97285a7de22eab09146ca5338778910fc
Orel <ryiseld@gmail.com>
2021-09-17 15:34:08 +0300

    Add text to show search query in search page

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -30,22 +30,27 @@ function SearchPage({ match }) {
         </div>
       )}
       {!loading && (
-        <div className="grid grid-cols-2 gap-10 md:pl-32 md:grid-cols-3 lg:grid-cols-4">
-          {results.map((result, i) => (
-            <Link to={`/game/${result.slug}`}>
-              <div
-                key={i}
-                className="relative w-48 text-center bg-center bg-no-repeat bg-cover rounded-lg shadow-md h-80"
-                style={{
-                  backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.8)), url(${
-                    result.cover ? result.cover.url : ""
-                  })`,
-                }}
-              >
-                <div className="absolute w-full text-lg font-bold bottom-5">{result.name}</div>
-              </div>
-            </Link>
-          ))}
+        <div>
+          <div className="pl-20 mb-10 text-xl font-light lg:pl-32 ">
+            Showing results for: <span className="font-normal">{match.params.name}</span>
+          </div>
+          <div className="grid grid-cols-1 gap-10 pl-20 lg:pl-32 md:grid-cols-3 lg:grid-cols-4">
+            {results.map((result, i) => (
+              <Link to={`/game/${result.slug}`}>
+                <div
+                  key={i}
+                  className="relative w-48 text-center bg-center bg-no-repeat bg-cover rounded-lg shadow-md h-80"
+                  style={{
+                    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.8)), url(${
+                      result.cover ? result.cover.url : ""
+                    })`,
+                  }}
+                >
+                  <div className="absolute w-full text-lg font-bold bottom-5">{result.name}</div>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       )}
     </>
